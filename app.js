414b5ec1529df521a282674b7335dc16e4a7b1ef
Toprak Tüzün <topraktuzun17@hotmail.com>
2023-03-27 16:26:28 +0300

    Photos send index page and show in

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const ejs = require('ejs');
 const app = express();
+const Photo = require('./models/Photo');
 
+//connect
+mongoose.connect('mongodb://127.0.0.1:27017/pcat-test-db');
+
+//Template engine
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  //res.sendFile(path.resolve(__dirname, 'temp/index.html'));
-  res.render('index');
+//mmiddlewares
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//routes
+app.get('/', async (req, res) => {
+  const photos= await Photo.find({});
+  //console.log(photos);
+  res.render('index',{
+    photos
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -19,6 +33,11 @@ app.get('/about', (req, res) => {
 app.get('/add', (req, res) => {
   //res.sendFile(path.resolve(__dirname, 'temp/index.html'));
   res.render('add');
+});
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
 });
 
 const port = 3000;
